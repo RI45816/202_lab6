e455d93eec5ea388759fe82965ebce850bc1ac50
RI45816 <uu3@umbc.edu>
2017-03-15 20:06:06 -0400

    first commit

---
+++ lab6.cpp
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*****************************************
+ ** File:    lab6.cpp
+ ** Project: CMSC 202 lab6, Spring 2017
+ ** Author:  Uzoma Uwanamodo
+ ** Date:    00/15/2017
+ ** Section: 05
+ ** E-mail:  uu3@umbc.edu
+ ** 
+ ** This file contains the main driver program for lab6.
+ ** 
+ ** 
+ ** 
+ ** 
+ *********************************************/
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
