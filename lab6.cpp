702674368a1b049fb4a94a7bf024c32fe51a2dff
RI45816 <uu3@umbc.edu>
2017-03-15 20:07:27 -0400

    JAT55

--- lab6.cpp
+++ lab6.cpp
@@ -1,33 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*****************************************
- ** File:    lab6.cpp
- ** Project: CMSC 202 lab6, Spring 2017
- ** Author:  Uzoma Uwanamodo
- ** Date:    00/15/2017
- ** Section: 05
- ** E-mail:  uu3@umbc.edu
- ** 
- ** This file contains the main driver program for lab6.
- ** 
- ** 
- ** 
- ** 
- *********************************************/
-
-#include <cstdlib>
-
+#include "Student.h"
+#include <iostream>
+#include <string>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
-    return 0;
+int main() {
+  Student s;
+  s.Display();
+  return 0;
 }
-
